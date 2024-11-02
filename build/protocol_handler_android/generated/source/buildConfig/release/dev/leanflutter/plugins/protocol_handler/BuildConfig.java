75c5f0c16a961adb1beb79641fd350a7808fe9f2
seele123o <xuchangchun77@Gmail.com>
2024-11-02 12:31:40 +0800

    Add files via upload

---
+++ build/protocol_handler_android/generated/source/buildConfig/release/dev/leanflutter/plugins/protocol_handler/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package dev.leanflutter.plugins.protocol_handler;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "dev.leanflutter.plugins.protocol_handler";
+  public static final String BUILD_TYPE = "release";
+}
